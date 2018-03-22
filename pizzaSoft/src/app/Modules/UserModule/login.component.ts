ae5dd43f76366493cc93c105a8b49d2c5f3f9407
Alef Silva <alef_silva07@hotmail.com>
2018-03-21 21:40:14 -0300

    Instalação do bootstrap. Organização dos modules Novo module

---
+++ pizzaSoft/src/app/Modules/UserModule/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-login-component',
+    templateUrl: 'app.login.component.html',
+    styleUrls: ['app.login.css']
+})
+export class LoginComponent{
+
+}
